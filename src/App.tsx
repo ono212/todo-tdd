e7dcb45c66cd80bd8c3216760355ad8f2f07335d
ono212 <qkrwjdals302@naver.com>
2023-08-11 02:43:17 +0900

    Feat: Todo입력, 목록 출력 기능 구현
    - Todo목록 출력 시 key애 id를 사용하기 위해 uuid 라이브러리 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import { Todo } from "./Todo";
 
 function App() {
-  return <div className="App">learn react</div>;
+  return (
+    <div className="App">
+      learn react
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Todo.tsx
@@ -0,0 +1,40 @@
+import { ChangeEvent, KeyboardEvent, useState } from "react";
+import { v4 as uuid } from "uuid";
+
+type TodoType = {
+  id: string;
+  content: string;
+};
+
+const Todo = () => {
+  const [todos, setTodos] = useState<TodoType[]>([]);
+  const [content, setContent] = useState<string>("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setContent(e.target.value);
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      const id = uuid();
+      setTodos([{ id, content }, ...todos]);
+    }
+  };
+
+  return (
+    <div>
+      <h2>todos</h2>
+      <input
+        type="text"
+        data-testid="todo-input"
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+      {todos.map((todo) => (
+        <div key={todo.id}>{todo.content}</div>
+      ))}
+    </div>
+  );
+};
+
+export { Todo };
