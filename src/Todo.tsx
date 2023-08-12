70164e21f36de7c0c9efb79930c4984441898f51
ono212 <qkrwjdals302@naver.com>
2023-08-12 22:14:35 +0900

    Refactor: todo데이터 관련 함수들 커스텀 훅으로 추출

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,44 +1,18 @@
-import { useState } from "react";
-import { TodoType } from "./types";
 import { TodoInput } from "./TodoInput";
 import { TodoList } from "./TodoList";
 import "./Todo.css";
+import { useTodos } from "./useTodos";
 
 const Todo = () => {
-  const [todos, setTodos] = useState<TodoType[]>([]);
-
-  const onItemAdded = (todo: TodoType) => setTodos([...todos, todo]);
-
-  const onToggleItem = (todo: TodoType) => {
-    setTodos(
-      todos.map((item) => {
-        if (item.id === todo.id) {
-          return { ...item, completed: !item.completed };
-        }
-        return item;
-      })
-    );
-  };
-
-  const onDeleteItem = (todo: TodoType) => {
-    setTodos(
-      todos.filter((item) => {
-        if (item.id === todo.id) {
-          console.log("삭제!");
-        }
-
-        return item.id !== todo.id;
-      })
-    );
-  };
+  const { todos, addTodo, toggleTodo, deleteTodo } = useTodos();
   return (
     <div className="todo-container">
       <h2>todos</h2>
-      <TodoInput onItemAdded={onItemAdded} />
+      <TodoInput onItemAdded={addTodo} />
       <TodoList
         todos={todos}
-        onToggleItem={onToggleItem}
-        onDeleteItem={onDeleteItem}
+        onToggleItem={toggleTodo}
+        onDeleteItem={deleteTodo}
       />
     </div>
   );
